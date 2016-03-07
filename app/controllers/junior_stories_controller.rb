8e51c2861f565da7bf01ead0d38d753feb78370a
TPei <t.s.peikert@gmail.com>
2016-03-07 14:41:41 +0100

    instead of loading attributes from model, I provide a hash with demo seearch terms to make this more usable

--- app/controllers/junior_stories_controller.rb
+++ app/controllers/junior_stories_controller.rb
@@ -4,10 +4,7 @@ class JuniorStoriesController < ApplicationController
     @junior_stories = JuniorStory.
       filter(params.except(:controller, :action, :format))
 
-    @fields = JuniorStory.attribute_names - ['id',
-                                             'publishing_consent',
-                                             'updated_at',
-                                             'created_at']
+    @fields = fields
 
     respond_to do |format|
       format.html
@@ -38,5 +35,34 @@ class JuniorStoriesController < ApplicationController
       :age, :years_working_in_total, :years_working_at_job, :education, :first_job,
       :remote, :tech_team_size, :company_size, :company_age, :person_of_colour, :other,
       :publishing_consent, :freelancer)
+  end
+
+  # TODO: shouldn't hard code this, but I like having demo search terms in the list
+  def fields
+    {
+      job: "junior developer",
+      happy_in_job: "yes",
+      happy_info: "It's ok",
+      gender: "female",
+      city: "Berlin",
+      country: "DE",
+      days_per_week: "5",
+      salary: "15000",
+      currency: "€",
+      technology: "RoR",
+      focus: "backend",
+      age: 30,
+      years_working_total: "less than 1 year",
+      years_working_at_job: "less than 1 year",
+      education: "self taught",
+      first_job: "no",
+      remote: "no",
+      tech_team_size: "5 - 10 people",
+      company_size: "less than 10 people",
+      company_age: 5,
+      person_of_colour: false,
+      other: "Thanks",
+      freelancer: "f"
+    }
   end
 end
