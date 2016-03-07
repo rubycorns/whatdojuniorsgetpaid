5db541ba34d16ad62c043d7249c5efa545a1c381
TPei <t.s.peikert@gmail.com>
2016-03-07 07:56:13 +0100

    adds tests for paramter filtering

--- spec/controllers/junior_stroy_controller_spec.rb
+++ spec/controllers/junior_stroy_controller_spec.rb
@@ -2,22 +2,52 @@ require 'rails_helper'
 
 describe JuniorStoriesController do
   describe 'GET index' do
-    it 'assigns @junior_stories' do
-      story = build :junior_story
-      story.save
-      get :index
-      expect(assigns(:junior_stories)).to eq([story])
+    context 'without filter params' do
+      it 'assigns @junior_stories' do
+        story = build :junior_story
+        story.save
+        get :index
+        expect(assigns(:junior_stories)).to eq([story])
+      end
+
+      it 'renders the index template' do
+        get :index
+        expect(response).to render_template('index')
+      end
+
+      it 'does not render index, but instead responds with csv' do
+        get :index, format: 'csv'
+        expect(response).not_to render_template('index')
+        expect(response.content_type).to include 'application/csv'
+      end
     end
 
-    it 'renders the index template' do
-      get :index
-      expect(response).to render_template('index')
-    end
+    context 'with filter params' do
+      it 'returns only the filter appropriate junior_story' do
+        female_story = build :junior_story
+        female_story.gender = "female"
+        female_story.save
 
-    it 'does not render index, but instead responds with csv' do
-      get :index, format: 'csv'
-      expect(response).not_to render_template('index')
-      expect(response.content_type).to include 'application/csv'
+        male_story = build :junior_story
+        male_story.gender = "male"
+        male_story.save
+
+
+        get :index, {"gender" => "female"}
+        expect(assigns(:junior_stories)).to eq([female_story])
+      end
+
+      it 'renders the index template' do
+        get :index, { "gender" => "female" }
+        get :index
+        expect(response).to render_template('index')
+      end
+
+      it 'does not render index, but instead responds with csv' do
+        get :index, { "gender" => "female", "format" => "csv" }
+        expect(response).not_to render_template('index')
+        expect(response.content_type).to include 'application/csv'
+      end
     end
   end
 end
