55d087df3ede959dd857502d7c66c5d564e180ce
TPei <t.s.peikert@gmail.com>
2016-03-07 08:05:14 +0100

    updates strings to use single quotes

--- spec/controllers/junior_stroy_controller_spec.rb
+++ spec/controllers/junior_stroy_controller_spec.rb
@@ -25,26 +25,26 @@ describe JuniorStoriesController do
     context 'with legal filter params' do
       it 'returns only the filter appropriate junior_story' do
         female_story = build :junior_story
-        female_story.gender = "female"
+        female_story.gender = 'female'
         female_story.save
 
         male_story = build :junior_story
-        male_story.gender = "male"
+        male_story.gender = 'male'
         male_story.save
 
 
-        get :index, { "gender" => "female" }
+        get :index, { 'gender' => 'female' }
         expect(assigns(:junior_stories)).to eq([female_story])
       end
 
       it 'renders the index template' do
-        get :index, { "gender" => "female" }
+        get :index, { 'gender' => 'female' }
         get :index
         expect(response).to render_template('index')
       end
 
       it 'does not render index, but instead responds with csv' do
-        get :index, { "gender" => "female", "format" => "csv" }
+        get :index, { 'gender' => 'female', 'format' => 'csv' }
         expect(response).not_to render_template('index')
         expect(response.content_type).to include 'application/csv'
       end
@@ -53,26 +53,26 @@ describe JuniorStoriesController do
     context 'with illegal filter params' do
       it 'returns only the filter appropriate junior_story' do
         female_story = build :junior_story
-        female_story.gender = "female"
+        female_story.gender = 'female'
         female_story.save
 
         male_story = build :junior_story
-        male_story.gender = "male"
+        male_story.gender = 'male'
         male_story.save
 
 
-        get :index, { "makes_no_sense" => "random" }
+        get :index, { 'makes_no_sense' => 'random' }
         expect(assigns(:junior_stories)).to eq([female_story, male_story])
       end
 
       it 'renders the index template' do
-        get :index, { "makes_no_sense" => "random" }
+        get :index, { 'makes_no_sense' => 'random' }
         get :index
         expect(response).to render_template('index')
       end
 
       it 'does not render index, but instead responds with csv' do
-        get :index, { "makes_no_sense" => "random", "format" => "csv" }
+        get :index, { 'makes_no_sense' => 'random', 'format' => 'csv' }
         expect(response).not_to render_template('index')
         expect(response.content_type).to include 'application/csv'
       end
